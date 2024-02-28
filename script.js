2f8d3a308f9aec4b36756a90019b3942891035c2
Melissa Tchamdja <tchamdjamel@gmail.com>
2024-02-28 14:33:57 +0100

    commit

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-document.getElementById('languageToggle').addEventListener('click', function() {
-    var buttonText = this.textContent;
-    this.textContent = buttonText === "Changer de langue" ? "Switch Language" : "Changer de langue";
-    // Implémentez ici la logique pour changer les textes des autres éléments du site si nécessaire
-});
+document.addEventListener('DOMContentLoaded', function () {
+    // Ensure the DOM is fully loaded before attaching the event listener
 
-// Ajoutez ici le code JavaScript pour gérer l'envoi du formulaire de contact
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Implémentez ici la logique pour envoyer les informations de contact, par exemple, via un API ou un service d'email
-    alert("Message envoyé !");
+    document.querySelector('.contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Retrieve form data
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        // Build FormData object
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('email', email);
+        formData.append('message', message);
+
+        // Formspree endpoint URL
+        const apiUrl = 'https://formspree.io/f/vpzokgey';
+
+        // Send data to Formspree using POST method
+        fetch(apiUrl, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Display a confirmation message to the user
+            alert('Formulaire soumis avec succès !');
+
+            // Reset the form
+            document.querySelector('.contact-form').reset();
+        })
+        .catch(error => {
+            console.error('Erreur lors de l\'envoi du formulaire :', error);
+            // Display an error message to the user
+            alert('Une erreur s\'est produite lors de l\'envoi du formulaire.');
+        });
+    });
 });
